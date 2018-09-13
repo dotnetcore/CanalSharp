3770b0b167e211343b0757a51b3781bb9a7ecfb0
WithLin <withlin@yeah.net>
2018-09-13 19:50:43 +0800

    fix add interface action

---
+++ src/Canal.Csharp.Abstract/Exception/CanalClientException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Canal.Csharp.Abstract.Exception
+{
+    public class CanalClientException:System.Exception
+    {
+        public CanalClientException(string errorCode):base(errorCode)
+        {
+        }
+
+        public CanalClientException(string errorCode, System.Exception cause):base(errorCode,cause)
+        {
+        }
+    }
+}
---
+++ src/Canal.Csharp.Protocol/Message.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Com.Alibaba.Otter.Canal.Protocol;
+using Google.Protobuf;
+
+namespace Canal.Csharp.Protocol
+{
+
+    public class Message
+    {
+        public long Id { get; set; }
+        public List<Entry> Entries { get; set; }
+
+        public bool Raw { get; set; }
+
+        public List<ByteString> RawEntries = new List<ByteString>();
+
+        public Message(long id, List<Entry> entries)
+        {
+            Id = id;
+            Entries = entries ?? new List<Entry>();
+            Raw = false;
+        }
+        public Message(long id, bool raw, object entries)
+        {
+            Id = id;
+            if (raw)
+            {
+                RawEntries = (List<ByteString>) (entries ?? new List<ByteString>());
+            }
+            else
+            {
+                Entries = (List<Entry>) (entries ?? new List<Entry>());
+            }
+        }
+    }
+}
