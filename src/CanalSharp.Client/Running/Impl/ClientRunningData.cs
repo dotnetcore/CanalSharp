639f6837f9c093ad6e6f6aea2899bc9fe3dcc91d
WithLin <withlin@yeah.net>
2018-09-15 09:44:21 +0800

    Fix add Running module

---
+++ src/CanalSharp.Client/Running/Impl/ClientRunningData.cs
@@ -0,0 +1,9 @@
+﻿namespace CanalSharp.Client.Running.Impl
+{
+    public class ClientRunningData
+    {
+        public short ClientId { get; set; }
+        public string Address { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
---
+++ src/CanalSharp.Client/Running/Impl/ClientRunningMonitor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Canal.Csharp.Abstract.Common;
+
+namespace Canal.Csharp.Core.Running
+{
+    public class ClientRunningMonitor: AbstractCanalLifeCycle
+    {
+    }
+}
