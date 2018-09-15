5e17fc0aba20d245989a40d4ed20a3819ba50094
WithLin <withlin@yeah.net>
2018-09-15 09:54:39 +0800

    Fix add utils module

---
+++ src/CanalSharp.Common/Utils/BooleanMutex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Canal.Csharp.Core.Common.Utils
+{
+   public class BooleanMutex
+    {
+        public void Set()
+        {
+            Mutex mutex =new Mutex();
+            mutex.ReleaseMutex();
+        }
+    }
+}
