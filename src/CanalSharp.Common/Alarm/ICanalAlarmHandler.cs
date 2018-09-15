270f12354e5c787653b111ac7d02efc17631ae4f
WithLin <withlin@yeah.net>
2018-09-15 09:38:26 +0800

    Fix add Alarm module

---
+++ src/CanalSharp.Common/Alarm/ICanalAlarmHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CanalSharp.Common.Alarm
+{
+    interface ICanalAlarmHandler
+    {
+    }
+}
---
+++ src/CanalSharp.Common/Alarm/Impl/LogAlarmHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CanalSharp.Common.Alarm.Impl
+{
+    public class LogAlarmHandler : ICanalAlarmHandler
+    {
+        private readonly ILogger _logger = LogManager.GetLogger(typeof(LogAlarmHandler));
+        public void SendAlarm(string destination, string msg)
+        {
+            _logger.Warning($"destination:{destination}[{msg}]");
+        }
+    }
+}
