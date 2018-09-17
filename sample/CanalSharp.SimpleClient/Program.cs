44a4b5b690c578e5805bb56ae2297d879ae07499
stulzq <stulzq@qq.com>
2018-09-17 22:44:44 +0800

    add sample project

---
+++ sample/CanalSharp.SimpleClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CanalSharp.Client;
+using CanalSharp.Client.Impl;
+
+namespace CanalSharp.SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = "example";
+            ICanalConnector connector = CanalConnectors.NewSingleConnector("127.0.0.1", 11111, destination, "", "");
+            connector.Connect();
+            Console.Read();
+        }
+    }
+}
