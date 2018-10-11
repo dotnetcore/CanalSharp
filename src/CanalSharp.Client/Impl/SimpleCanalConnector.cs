df35ab6052fe577f71b1521839906ec418ed8d70
WithLin <withlin@yeah.net>
2018-10-11 18:24:31 +0800

    Fix rename the filed

--- src/CanalSharp.Client/Impl/SimpleCanalConnector.cs
+++ src/CanalSharp.Client/Impl/SimpleCanalConnector.cs
@@ -76,9 +76,9 @@ namespace CanalSharp.Client.Impl
 
         public int Port { get; set; }
 
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
-        public string Password { get; set; }
+        public string PassWord { get; set; }
 
         /// <summary>
         ///  // milliseconds
