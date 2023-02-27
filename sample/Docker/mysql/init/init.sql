1e6424a6d46f20df1beaa947ea75fda160d8de1a
stulzq <stulzq@qq.com>
2023-02-27 19:59:03 +0800

    feat: add docker mysql

---
+++ sample/Docker/mysql/init/init.sql
@@ -0,0 +1,22 @@
+
+CREATE USER canal IDENTIFIED BY 'canal';  
+GRANT ALL PRIVILEGES ON *.* TO 'canal'@'%' ;
+FLUSH PRIVILEGES;
+
+CREATE DATABASE `test`;
+
+USE test;
+
+
+CREATE TABLE `test` (
+        `id` INT(11) NOT NULL,
+        `name` VARCHAR(50) NOT NULL
+)
+ENGINE=InnoDB
+;
+
+ALTER TABLE `test`.`test`
+    ADD PRIMARY KEY (`id`);
+
+
+
